fa0118b39428bb914ab36b68e7a816eb21805ab7
WesleySquire <Wesley.Squire@keyin.com>
2022-04-11 15:57:07 -0230

    Commit2

---
+++ readme.txt
@@ -0,0 +1,3 @@
+Added a few repositories
+Added some css and partials
+Registering a user now saves it to users.json but reading from there yet
--- routes/home.js
+++ routes/home.js
@@ -7,16 +7,19 @@ const passport = require('passport');
 const flash = require('express-flash');
 const session = require('express-session');
 const methodOverride = require('method-override');
+const fs = require('fs');
+const fsPromise = require('fs').promises;
+const crc32 = require('crc/crc32');
 
 const initializePassport = require('../services/passport-config');
 initializePassport(
   passport,
-  (email) => users.find((user) => user.email === email),
-  (id) => users.find((user) => user.id === id)
+  (email) => users1.find((user) => user.email === email),
+  (id) => users1.find((user) => user.id === id)
 );
 
 // Can connect to database here, perhaps a tokens.json?!
-const users = [];
+const users1 = [];
 
 // Had to use app.use instead of router.use as running router would cause errors w/ passport and our authenticated functions
 app.use(express.static('public'));
@@ -60,11 +63,42 @@ app.get('/register', checkNotAuthenticated, (req, res) => {
 app.post('/register', checkNotAuthenticated, async (req, res) => {
   try {
     const hashedPassword = await bcrypt.hash(req.body.password, 10);
-    users.push({
-      id: Date.now().toString(),
+    let crc = crc32(req.body.name).toString(16);
+    let now = new Date();
+    let name = req.body.name;
+    let email = req.body.email;
+    users1.push({
+      id: crc,
       name: req.body.name,
       email: req.body.email,
       password: hashedPassword,
+    });
+    fs.readFile('users.json', 'utf-8', function (err, data) {
+      if (err) throw err;
+      // This is setting the data to an array var
+      var array = JSON.parse(data);
+      // We're pushing the inputed data into the tokens array inside of the
+      // data
+      array.users.push({
+        created: now,
+        name: name,
+        email: email,
+        password: hashedPassword,
+      });
+
+      console.log(array);
+      // This re-writes the json folder with the newly added token
+      fs.writeFile(
+        'users.json',
+        JSON.stringify(array),
+        'utf-8',
+        function (err) {
+          if (err) {
+            console.log(err);
+            throw err;
+          }
+        }
+      );
     });
     res.redirect('/login');
   } catch {
