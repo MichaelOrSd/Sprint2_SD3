1419218c73bbe039515bacab1e23e3c349a8b6ca
WesleySquire <Wesley.Squire@keyin.com>
2022-04-12 19:29:03 -0230

    Woops LOL

--- services/usersDb.js
+++ services/usersDb.js
@@ -2,8 +2,8 @@ const Pool = require('pg').Pool;
 
 const pool = new Pool({
   // user and password have to be set to an appropriate user to someones pgAdmin
-  user: 'user',
-  password: 'user',
+  user: 'postgres',
+  password: 'postgres',
   host: 'localhost',
   // selects what db to acess
   database: 'users',
